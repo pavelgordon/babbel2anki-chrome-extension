60b87b6c4baac574137e675dfb3b664d8e308c53
Pavel Gordon <p.gordon@mytaxi.com>
2020-04-29 11:00:51 +0200

    call anki connect to add notes

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -9,45 +9,6 @@
 //
 // });
 
-var xhrOverrideScript = document.createElement('script');
-xhrOverrideScript.type = 'text/javascript';
-xhrOverrideScript.innerHTML = `
-  (function() {
-    var XHR = XMLHttpRequest.prototype;
-    var send = XHR.send;
-    var open = XHR.open;
-    XHR.open = function(method, url) {
-        this.url = url; // the request url
-        return open.apply(this, arguments);
-    }
-    XHR.send = function() {
-        this.addEventListener('load', function() {
-            if (this.url.includes('learned_items')) {
-                var data = JSON.parse(this.response);
-                data.url = this.url
-                console.log('-------------------------------')
-                console.log(this)
-                console.log(data)
-                var dataDOMElement = document.createElement('div');
-                dataDOMElement.id = '__interceptedData';
-                dataDOMElement.innerText = JSON.stringify(data);
-                dataDOMElement.style.height = 0;
-                dataDOMElement.style.overflow = 'hidden';
-                document.body.appendChild(dataDOMElement);
-            }               
-        });
-        return send.apply(this, arguments);
-    };
-  })();
-  `
-document.head.prepend(xhrOverrideScript);
-
-
-
-
-
-
-
 
 function interceptData() {
   var xhrOverrideScript = document.createElement('script');
@@ -67,8 +28,8 @@ function interceptData() {
                 var data = JSON.parse(this.response);
                 data.url = this.url
                 console.log('-------------------------------')
-                console.log(this)
-                console.log(data)
+                console.log("setting __interceptedData this", this)
+                console.log("setting __interceptedData data", data)
                 var dataDOMElement = document.createElement('div');
                 dataDOMElement.id = '__interceptedData';
                 dataDOMElement.innerText = JSON.stringify(data);
@@ -84,6 +45,7 @@ function interceptData() {
   document.head.prepend(xhrOverrideScript);
 }
 
+
 function checkForDOM() {
   if (document.body && document.head) {
     interceptData();
@@ -95,30 +57,28 @@ function checkForDOM() {
 
 requestIdleCallback(checkForDOM);
 
-function scrapeData() {
+async function scrapeData() {
   var responseContainingEle = document.getElementById('__interceptedData');
   if (responseContainingEle) {
-    console.log("json data ", responseContainingEle.innerHTML)
-
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://localhost:8080/dictionary/download', true);
-    xhr.setRequestHeader('Content-type', 'application/json');
-    xhr.onload = function () {
-      console.log("babbel2anki exporter response", this.response);
-      const data = JSON.parse(this.response)
-      chrome.runtime.sendMessage('', {
-        type: 'notification',
-        options: {
-          title: `Processed ${data.new} new words`,
-          message: `${data.status}\nTotal words: ${data.total}`,
-          iconUrl: 'images/icon.png',
-          type: 'basic'
-        }
+    chrome.runtime.sendMessage(
+      {
+        action: "addNotes",
+        learnedItems: JSON.parse(responseContainingEle.innerHTML).learned_items,
+        deckName: 'test',
+        modelName: 'newModelName2'
+      }, function (response) {
+        console.log("forwardCards response", response);
+        chrome.runtime.sendMessage('', {
+          type: 'notification',
+          options: {
+            title: `Added ${response.addedNotes} new words`,
+            message: `\nTotal words: ${response.totalNotes}`,
+            iconUrl: 'images/icon.png',
+            type: 'basic'
+          }
+        });
       });
-    };
 
-
-    xhr.send(responseContainingEle.innerHTML);
 
     responseContainingEle.remove()
   }
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -10,3 +10,5 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }, false);
 }, false);
+
+
