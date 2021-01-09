105b6ccf10c484b6248581cf10a39872a6cd79ee
freemahn <gordon.pav@gmail.com>
2021-01-09 22:26:38 +0100

    feat: error handling, sync animation

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,70 +1,95 @@
 'use strict';
 
 
+function checkAnkiConnection() {
+  checkConnection()
+    .then(() => {
+      console.log("AnkiConnection UP");
+      chrome.storage.sync.set({ankiConnectionStatus: {ok: true, date: "now"}}, function () {
+      });
+    })
+    .catch(() => {
+      console.log("AnkiConnection DOWN");
+      chrome.storage.sync.set({ankiConnectionStatus: {ok: false, date: "now"}}, function () {
+      });
+    })
+  setTimeout(checkAnkiConnection, 10_000);
+}
+
+checkAnkiConnection();
 
 chrome.runtime.onMessage.addListener(
   (request, sender, sendResponse) => {
-    if (request.action === "addNotes") {
-      const deckName = request.deckName
-      const modelName = request.modelName
-      const tagString = request.tagString
-      const notes = request.learnedItems.map(
-        item => ({
-          deckName: deckName,
-          modelName: modelName,
-          fields: {
-            Word: item.learnLanguageText,
-            Picture: item.image && item.image.id
-              ? `<img src='https://images.babbel.com/v1.0.0/images/${item.image.id}/variations/square/resolutions/500x500.png'/>`
-              : '',
-            "Extra Info": item.displayLanguageText
-          },
-          options: {
-            "allowDuplicate": true,
-            "duplicateScope": "deck"
-          },
-          tags: tagString
-            ? tagString.split(',').map(s => s.trim())
-            : [],
-          audio: [{
-            url: `https://sounds.babbel.com/v1.0.0/sounds/${item.sound.id}/normal.mp3`,
-            filename: item.sound.id,
-            fields: [
-              "Pronunciation"
-            ]
-          }]
-
-        })
-      );
-      // console.log(`deck ${deckName}, model ${modelName}, notes:`, notes)
-      createDeck(deckName)
-        .then(() => createModel(modelName))
-        .then(() => addNotes(notes))
-        .then(response => (
-          {
-            notesIds: response.result,
-            addedNotes: response.result.filter(e => e != null).length,
-            totalNotes: response.result.length,
-            error: response.error
-          })
-        )
-        .then(result => sendNotification(result))
-        .then(result => sendResponse(result))
+    if (request.action !== "addNotes") {
+      return true
     }
+
+    getOrInitProperty("ankiConnectionStatus", {ok: false})
+      .then(ankiConnectionStatus => {
+        if (!ankiConnectionStatus.ok) {
+          sendResponse({addedNotes: -1, totalNotes: -1, error: "Anki is not reachable"})
+          return Promise.reject()
+        }
+      })
+      .then(t => {
+        const deckName = request.deckName
+        const modelName = request.modelName
+        const tagString = request.tagString
+        const notes = request.learnedItems.map(
+          item => ({
+            deckName: deckName,
+            modelName: modelName,
+            fields: {
+              Word: item.learnLanguageText,
+              Picture: item.image && item.image.id
+                ? `<img src='https://images.babbel.com/v1.0.0/images/${item.image.id}/variations/square/resolutions/500x500.png'/>`
+                : '',
+              "Extra Info": item.displayLanguageText
+            },
+            options: {
+              "allowDuplicate": true,
+              "duplicateScope": "deck"
+            },
+            tags: tagString
+              ? tagString.split(',').map(s => s.trim())
+              : [],
+            audio: [{
+              url: `https://sounds.babbel.com/v1.0.0/sounds/${item.sound.id}/normal.mp3`,
+              filename: item.sound.id,
+              fields: [
+                "Pronunciation"
+              ]
+            }]
+
+          })
+        );
+        createDeck(deckName)
+          .then(() => createModel(modelName))
+          .then(() => addNotes(notes))
+          .then(response => (
+            {
+              notesIds: response.result,
+              addedNotes: response.result.filter(e => e != null).length,
+              totalNotes: response.result.length,
+              error: response.error
+            })
+          )
+          .then(result => showNotification(result))
+          .then(result => sendResponse(result))
+      })
+
+
     return true
   })
-;
 
 
 function createDeck(deckName) {
-  // console.log(`createDeck ${deckName}`)
   return callAnkiConnect("createDeck", {
     deck: deckName
   })
 }
 
 function createModel(modelName) {
-  // console.log(`addModel ${modelName}`)
   return callAnkiConnect("createModel", {
     modelName: modelName,
     inOrderFields: ["Word", "Picture", "Extra Info", "Pronunciation"],
@@ -124,6 +149,13 @@ function createModel(modelName) {
 
 }
 
+function checkConnection() {
+  return fetch('http://127.0.0.1:8765',
+    {
+      method: 'POST',
+      body: JSON.stringify({action: "version", version: 6})
+    })
+}
 
 function addNotes(notes) {
   // console.log("addNotes", notes.length)
@@ -140,7 +172,7 @@ async function callAnkiConnect(action, params = {}, version = 6,) {
   return response.json()
 }
 
-function sendNotification(result) {
+function showNotification(result) {
   const options = {
     title: `Added ${result.addedNotes} new words`,
     message: `\nTotal words: ${result.totalNotes}`,
@@ -150,4 +182,19 @@ function sendNotification(result) {
   }
   chrome.notifications.create('', options);
   return result
+}
+
+
+function getOrInitProperty(property, defaultValue) {
+  return new Promise((resolve) => {
+      chrome.storage.sync.get([property], result => {
+        if (result[property] === undefined || result[property] == null) {
+          result[property] = defaultValue
+          chrome.storage.sync.set({property: defaultValue},
+            () => console.log(`initiated property ${property} with value ${defaultValue}`));
+        }
+        resolve(result[property])
+      })
+    }
+  )
 }
--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,16 +1,42 @@
 'use strict';
 
+
+let button, helpLink;
+
 //waits for DOM to load and then injects `Sync with Anki` button into web page.
 function injectSyncButton() {
-  if (document.body && document.head && document.getElementsByClassName('learned-items__toolbar-item')[1]) {
+  if (document.body && document.head
+    && document.getElementsByClassName('learned-items__toolbar-item')[1]
+    && document.getElementsByClassName('learned-items__toolbar-item')[1].children.length >= 1
+  ) {
     const sibling = document.getElementsByClassName('learned-items__toolbar-item')[1];
-    const button = document.createElement("a");
+    sibling.style.display = "flex"
+    document.getElementsByClassName("learned-items__toolbar")[0].style.display = "flex"
+    document.getElementsByClassName("learned-items__toolbar")[0].style.alignItems = "end"
+
+    const div = document.createElement("div");
+    div.id = "wrapperDiv"
+    div.style.display = "flex"
+    div.style.flexDirection = "column"
+
+    button = document.createElement("a");
     button.className = 'btn btn--primary'
-    button.innerHTML = 'Sync page with Anki'
-    button.addEventListener('click', () => {
-      syncToAnki()
-    });
-    sibling.appendChild(button)
+    button.style.marginRight = ".938rem"
+
+    button.innerHTML =
+      '<img id="sync_spinner" src="https://icongr.am/clarity/sync.svg?size=20&color=FFFFFF" style="margin-right: 8px" alt="spin me baby"/>' +
+      '<span id="sync_btn_label">Sync to Anki</span>'
+    button.addEventListener('click', syncHandler);
+
+    helpLink = document.createElement("a");
+    helpLink.className = 'helpLink'
+    helpLink.href = "https://github.com/pavelgordon/babbel2anki-chrome-extension#more-detailed-guide"
+    helpLink.innerText = "Troubleshoot"
+
+    div.append(button)
+    div.append(helpLink)
+    sibling.appendChild(div)
+
   } else {
     requestIdleCallback(injectSyncButton);
   }
@@ -19,16 +45,48 @@ function injectSyncButton() {
 requestIdleCallback(injectSyncButton);
 
 
-function syncToAnki() {
+async function ankiConnectionHealthCheck() {
+  if (!button) {
+    setTimeout(ankiConnectionHealthCheck, 100);
+    return
+  }
+
+  const ankiConnectionStatus = await getOrInitProperty("ankiConnectionStatus", {ok: false})
+  if (!ankiConnectionStatus.ok) {
+    console.log("Can't connect to Anki")
+    button.classList.add("disabled");
+    helpLink.style.display = "inline-flex"
+
+  } else {
+    console.log("Anki is up")
+    button.classList.remove("disabled");
+    helpLink.style.display = "none"
+  }
+  setTimeout(ankiConnectionHealthCheck, 10_000);
+}
+
+ankiConnectionHealthCheck();
+
+async function syncHandler() {
+
+  const ankiConnectionStatus = await getOrInitProperty("ankiConnectionStatus", {ok: false})
+  if (!ankiConnectionStatus.ok) {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+    return
+  }
+
   const state = JSON.parse(sessionStorage.getItem('review_manager_state'))
   const learnedItems = state.learnedItems.data
   console.log('Sending ', learnedItems.length, ' words to Anki. Might take a minute or two to load images and sounds for every word')
-  sendDataToAnki(learnedItems)
+  sendVocabularyToAnki(learnedItems)
 }
 
 
+async function sendVocabularyToAnki(learnedItems) {
+  //todo maybe start earlier?
+  startSpinningAnimation()
 
-async function sendDataToAnki(learnedItems) {
   const deckName = await getOrInitProperty('deckName', 'BabbelDeck')
   const modelName = await getOrInitProperty('modelName', 'BabbelModel')
   const tagString = await getOrInitProperty('tagString', '')
@@ -42,9 +100,10 @@ async function sendDataToAnki(learnedItems) {
       tagString: tagString
     }, response => {
       console.log(`Added ${response.addedNotes}/${response.totalNotes} new words to ${deckName} using model ${modelName} with tags (${tagString})`);
+
+      stopSpinningAnimation()
     });
 }
-
 
 
 function getOrInitProperty(property, defaultValue) {
@@ -60,3 +119,12 @@ function getOrInitProperty(property, defaultValue) {
     }
   )
 }
+
+function startSpinningAnimation() {
+  document.getElementById("sync_spinner").style.animation = "spin 2s linear infinite"
+}
+
+function stopSpinningAnimation() {
+  document.getElementById("sync_spinner").style.animation = ""
+}
+
